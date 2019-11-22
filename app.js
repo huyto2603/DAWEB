fa38436dc285da86f95609d427c46f11f77f81d3
Kevin's world <hunghuynh1412@gmail.com>
2019-11-22 20:51:55 +0700

    add image and change location of file image and style

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+var path = require('path');
 var app = express();
- 
+var url = require("url");
+app.use(express.static(__dirname+'/public'));
+//app.use(express.static(path.join(__dirname, '/style')));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({
     defaultLayout: 'main.handlebars',
     layoutsDir: 'views/layouts/'
@@ -14,6 +17,12 @@ app.get('/', function (req, res) {
 });
 app.get('/Acer-Nitro-5-AN515-54-58TJ', function (req, res) {
     res.render('an515-54-58TJ.handlebars');
+});
+app.get('/May-tinh-xach-tay', function (req, res) {
+    res.render('may-tinh-xach-tay.handlebars');
+});
+app.get('/ho-tro', function (req, res) {
+    res.render('contact.handlebars');
 });
 app.listen(3000,()=>{
     console.log('Web Server is running at http://localhost:3000');
